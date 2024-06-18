0589605e2d0d05528d0f54f12b953452c6d292a7
evaldo <evaaldormj@gmail.com>
2024-06-17 22:22:26 -0300

    Feat: Criada a navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import { SidebarComponent } from './Components/sidebar/sidebar.component';
   imports: [
     CommonModule,
     RouterOutlet,
-    SidebarComponent
+    SidebarComponent,
+    NavbarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
