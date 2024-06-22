9cca291e1790b7ba648cd403224178fdb6e46bc0
evaldo <evaaldormj@gmail.com>
2024-06-22 11:32:16 -0300

    Feat: Adicionada alteração de cor por link na sidebar; novos links na sidebar; nova table em ususarios

--- src/app/Components/sidebar/sidebar.component.ts
+++ src/app/Components/sidebar/sidebar.component.ts
@@ -1,14 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
   isSidebarOpen: boolean = false;
+
+  constructor(private router:Router) {}
+
+  isActive(url: string): boolean {
+    return url === this.router.url;
+  }
 
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen
