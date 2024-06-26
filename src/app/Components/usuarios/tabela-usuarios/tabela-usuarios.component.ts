d7a6ac9b4ba69ba0bd754235b2ffcbd87628cd9d
evaldo <evaaldormj@gmail.com>
2024-06-26 17:14:28 -0300

    Feat: Exportação de CSV

--- src/app/Components/usuarios/tabela-usuarios/tabela-usuarios.component.ts
+++ src/app/Components/usuarios/tabela-usuarios/tabela-usuarios.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component, QueryList, ViewChildren } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnChanges, QueryList, SimpleChanges, ViewChildren } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import usuarios from '../../../usuarios.json'
 import { CommonModule } from '@angular/common';
@@ -30,6 +30,9 @@ export class TabelaUsuariosComponent implements AfterViewInit {
   filtroSetor: string = "";
   usuariosLista: Usuario[] = usuarios;
   totalLinhas: number = 0;
+  totalPaginas: number = this.paginas.length;
+  paginaAtual: number = 1;
+  itensPorPagina: number = 20;
 
   @ViewChildren("usuarioRows") usuarioRows!: QueryList<any>
 
@@ -45,10 +48,45 @@ export class TabelaUsuariosComponent implements AfterViewInit {
   }
 
   usuarioFiltrado(): Usuario[] {
-    return this.usuariosLista.filter(usuario => {
+      return this.usuariosLista.filter(usuario => {
       const setorFiltrado = this.filtroSetor ? usuario.setor === this.filtroSetor : true;
       const textoFiltrado = this.filtroTexto ? usuario.nome.toLowerCase().includes(this.filtroTexto.toLowerCase()) : true;
       return setorFiltrado && textoFiltrado;
     })
+  }
+
+  mudarPagina(pagina: number): void {
+    this.paginaAtual = pagina;
+  }
+
+  paginas(): number[] {
+    return Array(Math.ceil(this.usuarioFiltrado().length / this.itensPorPagina)).fill(0).map((x,i) => i + 1);
+  }
+
+  usuariosPaginados(): Usuario[] {
+    const inicio = (this.paginaAtual - 1) * this.itensPorPagina;
+    const final = inicio + this.itensPorPagina;
+    return this.usuarioFiltrado().slice(inicio,final);
+  }
+
+  exportarCSV() {
+    const data = usuarios;
+    const replacer = (key: string, value: any) => (value === null || value === "" ? "S/V" : value);
+    const header = Object.keys(data[0]) as Array<keyof Usuario>;
+    const csv = data.map(row =>
+      header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(",")
+    );
+    csv.unshift(header.join(','));
+    const csvArray = csv.join('\r\n');
+
+    const a = document.createElement("a");
+    const blob = new Blob([csvArray], { type: "text/csv" });
+    const url = window.URL.createObjectURL(blob);
+
+    a.href = url;
+    a.download = "USUARIOS.csv";
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove;
   }
 }
